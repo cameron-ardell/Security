2949b7aa152599b7ffcb188c97eb8ca65c1b2fe0
coryalini <calini@dhcp-208-99.wireless.bowdoin.edu>
2016-01-30 13:58:04 -0500

    hopefully fixed issue

--- Security/ViewController.swift
+++ Security/ViewController.swift
@@ -71,60 +71,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     
-   /*
-    //trying to send stuff
-    @IBAction func submitAction(sender: AnyObject) {
-        
-        //var parameter = ["name" : nametextField.text, "ID": idtextField.text]
-        let parameters = ["test phrase":"Hello world"]
-        
-        
-        //create the url to send things to
-        let url = NSURL(string: "http://servename.com/api")
-        
-        //created object for session
-        let session = NSURLSession.sharedSession()
-        
-        //now making request using the url object
-        let request = NSMutableURLRequest(URL: url!)
-        request.HTTPMethod = "POST" //set http method as POST
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.addValue("application/json", forHTTPHeaderField: "Accept")
-        
-
-        do {
-            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(parameters, options: [])
-        } catch {
-            print(error)
-        }// pass dictionary to nsdata object and set it as request body
-        
-        
-        let task = session.dataTaskWithRequest(request) { data, response, error in guard data != nil else {
-                print("no data found: \(error)")
-                return
-            }
-            
-            // fails easily if server error
-            do {
-                if let json = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
-                    let success = json["success"] as? Int                                  // Okay, the `json` is here, let's get the value for 'success' out of it
-                    print("Success: \(success)")
-                } else {
-                    let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)    // No error thrown, but not NSDictionary
-                    print("Error could not parse JSON: \(jsonStr)")
-                }
-            } catch let parseError {
-                print(parseError)                                                          // Log the error thrown by `JSONObjectWithData`
-                let jsonStr = NSString(data: data!, encoding: NSUTF8StringEncoding)
-                print("Error could not parse JSON: '\(jsonStr)'")
-            }
-        }
-        
-        task.resume()
-        
+    @IBAction func tester(sender: AnyObject) {
+        echoTest()
     }
-    */
-
+    
+    func echoTest(){
+        var messageNum = 0
+        let ws = WebSocket("ws://localhost:8080")
+        let send : ()->() = {
+            let msg = "\(++messageNum): \(NSDate().description)"
+            print("send: \(msg)")
+            ws.send(msg)
+        }
+        ws.event.open = {
+            print("opened")
+            send()
+        }
+        ws.event.close = { code, reason, clean in
+            print("close")
+        }
+        ws.event.error = { error in
+            print("error \(error)")
+        }
+    }
 
 }
 
